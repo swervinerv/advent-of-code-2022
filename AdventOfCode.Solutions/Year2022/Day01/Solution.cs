dc4566f138a9f9c44ad6be393f40f08cf326ba16
Evan Johnston <evan.johnston@aviture.us.com>
2022-12-01 09:43:12 -0800

    feat: Solution 1 for Day 1

--- AdventOfCode.Solutions/Year2022/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day01/Solution.cs
@@ -2,11 +2,35 @@ namespace AdventOfCode.Solutions.Year2022.Day01;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(01, 2022, "") { }
+    public Solution() : base(01, 2022, "Calorie Counting") { }
 
     protected override string SolvePartOne()
     {
-        return "";
+        var inputList = Input.Split(
+            new string[] { "\n" },
+            StringSplitOptions.None
+        );
+
+        int maxCalories = 0;
+        int totalCalories = 0;
+        foreach (var entry in inputList)
+        {
+            if (string.IsNullOrWhiteSpace(entry))
+            {
+                if (totalCalories > maxCalories)
+                {
+                    maxCalories = totalCalories;
+                }
+
+                totalCalories = 0;
+
+                continue;
+            }
+
+            totalCalories += int.Parse(entry);
+        }
+
+        return maxCalories.ToString();
     }
 
     protected override string SolvePartTwo()
