a5321da5654f3c84f072f019e364823a790e7dcc
Evan Johnston <evan.johnston@aviture.us.com>
2022-12-03 12:52:48 -0800

    feat: Solution 2 for Day 3

--- AdventOfCode.Solutions/Year2022/Day03/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day03/Solution.cs
@@ -7,20 +7,7 @@ class Solution : SolutionBase
     protected override string SolvePartOne()
     {
         var inputList = Input.SplitByNewline();
-
-        var lowerCaseValues = Enumerable.Range(1, 26);
-        var upperCaseValues = Enumerable.Range(27, 26);
-
-        var caseValueMap = new Dictionary<string, int>();
-        foreach (var lowerCase in lowerCaseValues)
-        {
-            caseValueMap.Add(Number2String(lowerCase, false), lowerCase);
-        }
-
-        foreach (var upperCase in upperCaseValues)
-        {
-            caseValueMap.Add(Number2String(upperCase - 26, true), upperCase);
-        }
+        var caseValueMap = GenerateCaseValueMap();
 
         var result = 0;
         foreach (var input in inputList)
@@ -44,7 +31,48 @@ class Solution : SolutionBase
 
     protected override string SolvePartTwo()
     {
-        return "";
+        var inputList = Input.SplitByNewline();
+        var caseValueMap = GenerateCaseValueMap();
+
+        var result = 0;
+        for (var x = 0; x < inputList.Count(); x += 3)
+        {
+            var itemOne = inputList.ElementAt(x);
+            var itemTwo = inputList.ElementAt(x + 1);
+            var itemThree = inputList.ElementAt(x + 2);
+
+            for (var y = 0; y < itemOne.Length; y++)
+            {
+                if (itemTwo.Contains(itemOne[y], StringComparison.CurrentCulture) && itemThree.Contains(itemOne[y], StringComparison.CurrentCulture))
+                {
+                    result += caseValueMap.GetValueOrDefault(itemOne[y].ToString());
+
+                    break;
+                }
+            }
+        }
+
+
+        return result.ToString();
+    }
+
+    private static Dictionary<string, int> GenerateCaseValueMap()
+    {
+        var lowerCaseValues = Enumerable.Range(1, 26);
+        var upperCaseValues = Enumerable.Range(27, 26);
+
+        var caseValueMap = new Dictionary<string, int>();
+        foreach (var lowerCase in lowerCaseValues)
+        {
+            caseValueMap.Add(Number2String(lowerCase, false), lowerCase);
+        }
+
+        foreach (var upperCase in upperCaseValues)
+        {
+            caseValueMap.Add(Number2String(upperCase - 26, true), upperCase);
+        }
+
+        return caseValueMap;
     }
 
     private static string Number2String(int number, bool isCaps)
