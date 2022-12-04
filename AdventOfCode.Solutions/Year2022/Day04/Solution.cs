3d1af6a08db546794cbae58cd9745ff2f3263e05
Evan Johnston <evan.johnston@aviture.us.com>
2022-12-04 14:41:20 -0800

    feat: Solution 2 for Day 4

--- AdventOfCode.Solutions/Year2022/Day04/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day04/Solution.cs
@@ -31,6 +31,26 @@ class Solution : SolutionBase
 
     protected override string SolvePartTwo()
     {
-        return "";
+        var inputList = Input.SplitByNewline();
+
+        var result = 0;
+        foreach (var input in inputList)
+        {
+            var rangeOne = input.Split(',')[0].Split('-').Select(_ => int.Parse(_)).ToArray();
+            var rangeTwo = input.Split(',')[1].Split('-').Select(_ => int.Parse(_)).ToArray();
+
+            var enumerableRangeOne = Enumerable.Range(rangeOne[0], rangeOne[1] - rangeOne[0] + 1);
+            var enumerableRangeTwo = Enumerable.Range(rangeTwo[0], rangeTwo[1] - rangeTwo[0] + 1);
+
+            var intersection = enumerableRangeOne.Intersect(enumerableRangeTwo);
+            var intersectionCount = intersection.Count();
+
+            if (intersectionCount > 0)
+            {
+                result++;
+            }
+        }
+
+        return result.ToString();
     }
 }
