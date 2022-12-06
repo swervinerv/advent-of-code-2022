2502a99a283cd79674231744f167f7467d376651
Evan Johnston <evan.johnston@aviture.us.com>
2022-12-06 09:47:57 -0800

    feat: Solution 1 for Day 6

--- AdventOfCode.Solutions/Year2022/Day06/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day06/Solution.cs
@@ -2,11 +2,34 @@ namespace AdventOfCode.Solutions.Year2022.Day06;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(06, 2022, "") { }
+    public Solution() : base(06, 2022, "Tuning Trouble") { }
 
     protected override string SolvePartOne()
     {
-        return "";
+        var inputBuffer = Input;
+        List<string> inputParser = new();
+        int result = 0;
+
+        for (var x = 0; x < inputBuffer.Length; x++)
+        {
+            inputParser.Add(inputBuffer.Substring(x, 1));
+            
+            if (inputParser.Count < 4)
+            {
+                continue;
+            }
+
+            if (inputParser.Distinct().Count() == 4)
+            {
+                result = x + 1;
+
+                break;
+            }
+
+            inputParser.RemoveAt(0);
+        }
+
+        return result.ToString();
     }
 
     protected override string SolvePartTwo()
