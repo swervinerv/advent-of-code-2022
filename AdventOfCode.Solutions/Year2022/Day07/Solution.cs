f4fce9bd59939974defa152a661900e56adf2ff1
Evan Johnston <evan.johnston@aviture.us.com>
2022-12-08 10:15:04 -0800

    feat: Solution 2 for Day 7

--- AdventOfCode.Solutions/Year2022/Day07/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day07/Solution.cs
@@ -113,7 +113,111 @@ internal class Solution : SolutionBase
 
     protected override string SolvePartTwo()
     {
-        return "";
+        var commands = Input.SplitByNewline();
+
+        Dictionary<int, string> commandList = new();
+        for (var x = 0; x < commands.Length; x++)
+        {
+            commandList.Add(x, commands[x]);
+        }
+
+        ElfFile baseFile = new()
+        {
+            Name = "/",
+            IsDirectory = true,
+        };
+        ElfFile currentDirectory = baseFile;
+
+        Dictionary<string, ElfFile> directoryHash = new();
+
+        int commandIndex = 0;
+        while (commandIndex < commands.Length)
+        {
+            var command = commandList[commandIndex];
+
+            if (command.StartsWith("$"))
+            {
+                var arguments = command.Split(' ');
+                if (arguments[1] == "cd")
+                {
+                    var directory = arguments[2];
+                    if (directory == "/")
+                    {
+                        currentDirectory = baseFile;
+                    }
+                    else if (directory == ".." && currentDirectory.Parent != null)
+                    {
+                        currentDirectory = currentDirectory.Parent;
+                    }
+                    else
+                    {
+                        var nextDirectory = currentDirectory.Children
+                            .FirstOrDefault(_ => _.Name == directory);
+
+                        if (nextDirectory == null)
+                        {
+                            throw new Exception("Unable to file child directory " + directory);
+                        }
+
+                        currentDirectory = nextDirectory;
+                    }
+
+                    commandIndex++;
+                }
+                else if (arguments[1] == "ls")
+                {
+                    var nextCommands = commandList
+                        .Where(_ => _.Key > commandIndex && _.Value.StartsWith("$"));
+
+                    var nextCommandIndex = nextCommands.Any()
+                        ? nextCommands.Min(_ => _.Key)
+                        : commands.Length;
+
+                    for (var x = commandIndex + 1; x < nextCommandIndex; x++)
+                    {
+                        var listResult = commandList[x];
+                        var fileName = listResult.Split(' ')[1];
+
+                        if (listResult.StartsWith("dir"))
+                        {
+                            ElfFile newDirectory = new()
+                            {
+                                Name = fileName,
+                                Parent = currentDirectory,
+                                IsDirectory = true
+                            };
+
+                            currentDirectory.Children.Add(newDirectory);
+                            directoryHash.Add(newDirectory.HashKey, newDirectory);
+                        }
+                        else
+                        {
+                            int fileSize = int.Parse(listResult.Split(' ')[0]);
+
+                            currentDirectory.Children.Add(new()
+                            {
+                                Name = fileName,
+                                Size = fileSize,
+                                IsDirectory = false
+                            });
+                        }
+                    }
+
+                    commandIndex = nextCommandIndex;
+                }
+            }
+        }
+
+        var totalSize = baseFile.GetSize();
+        var currentUnused = 70000000 - totalSize;
+        var minDirectorySizeToDelete = 30000000 - currentUnused;
+
+        var directoryToDelete = directoryHash
+            .Where(_ => _.Value.GetSize() >= minDirectorySizeToDelete)
+            .OrderBy(_ => _.Value.GetSize())
+            .FirstOrDefault();
+
+        return directoryToDelete.Value.GetSize().ToString();
     }
 
     private class ElfFile
@@ -128,10 +232,8 @@ internal class Solution : SolutionBase
             => $"{Name}-{Parent?.Name ?? string.Empty}";
 
         public int GetSize()
-        {
-            return IsDirectory
+            => IsDirectory
                 ? Children.Sum(_ => _.GetSize())
                 : Size;
-        }
     }
 }
